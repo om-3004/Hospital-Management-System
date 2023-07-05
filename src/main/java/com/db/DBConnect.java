d744dee5f61ad563f031409bd54566ec015ada90
Om Thakkar <76418145+om-3004@users.noreply.github.com>
2023-07-05 09:08:42 +0530

    Update DBConnect.java

--- src/main/java/com/db/DBConnect.java
+++ src/main/java/com/db/DBConnect.java
@@ -11,6 +11,7 @@ public class DBConnect {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
+			// replace password by the password set on your mysql workbench....
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital", root, password);
 		} catch (Exception e) {
 			e.printStackTrace();
